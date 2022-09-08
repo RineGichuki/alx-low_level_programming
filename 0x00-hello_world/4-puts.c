6f518642f00e71a9898ac736149825fdfd68de70
RineGichuki <wanjugucarolineg020@gmail.com>
2022-09-08 10:00:01 -0700

    Fourthe commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  * * main - print the string in the put function
@@ -20,7 +18,7 @@ int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
 
