0a4a4b8aa7170268569596a76e66a04347d03ce8
RineGichuki <wanjugucarolineg020@gmail.com>
2022-09-09 08:53:20 -0700

    thirds task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+/* more headers go there */
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char str;
+
+
+
+			for (str = 'a'; str <= 'z' ; str++)
+
+						putchar(str);
+
+				putchar('\n');
+
+					return (0);
+
+
+
+}
