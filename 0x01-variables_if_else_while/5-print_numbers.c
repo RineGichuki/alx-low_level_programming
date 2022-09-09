92e77ad739437ff38a0060df091faedd6c01ed9e
RineGichuki <wanjugucarolineg020@gmail.com>
2022-09-09 09:24:08 -0700

    fifth task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.>
+#include <ctype.h>
+
+/* more headers go there */
+
+/**
+ * main - Entry point
+ i*    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	putchar('\n');
+	return (0);
+}
